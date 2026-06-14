34029c90f93cc2fa37abf36b16ff70a01a871a62
rogerkorantenng <rogerkorantenng@gmail.com>
2026-06-14 03:03:12 +0000

    chore: remove package_app.sh and DEVPOST_SUBMISSION.md from repo (keep local only)

--- package_app.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-cd /home/rogerkorantenng/dev/Hackathons/sankofa
-tar -czf sankofa.spl \
-  --transform 's|^splunk_app|sankofa|' \
-  splunk_app/
-echo "Created sankofa.spl"
-ls -lh sankofa.spl
